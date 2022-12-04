50d43f5df9f4f6a85d5f7ba4730f263dae4745e1
Jan Trautmann <jan.trautmann@mpq.mpg.de>
2022-12-04 18:47:14 +0100

    added abstract class and dummy class

--- src/piconet/webui.py
+++ src/piconet/webui.py
@@ -2,11 +2,11 @@ from .pico_device import Ps2000Device
 import qcontrol4.gui.web.widgets as qw
 import asyncio
 import matplotlib.pyplot as plt
-from matplotlib import Figure
 import numpy as np
 import logging
 import sys
 from typing import Optional, Callable, Union
+from abc import abstractmethod, ABCMeta
 
 """
 To do:
@@ -15,6 +15,64 @@ To do:
 - add doc strings
 - add types
 """
+
+class AbstractScope(metaclass=ABCMeta):
+    @abstractmethod
+    def update(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def capture_coro(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def singleshot(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def start_capture(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def stop_capture(self):
+        raise NotImplementedError()
+
+
+
+class DummyScope(AbstractScope):
+    def __init__(self):
+        self.ch_range = 0.1
+        self._running = False
+
+    def update(self):
+        return
+
+    async def capture_coro(self):
+        n = 50
+        while self._running:
+            if self._capture_cb:
+                x = np.linspace(0, 5, n)
+                y = x**2 + np.rand.normal(0, 1, n)
+                fig, ax = plt.subplots(figsize=(1000 / 72, 400 / 72))
+                ax.plot(x, y)
+                self._capture_cb(fig)
+
+    def singleshot(self):
+        x = np.linspace(0, 5, 20)
+        y = np.linspace(0, 5, 20)
+        fig, ax = plt.subplots(figsize=(1000 / 72, 400 / 72))
+        ax.plot(x, y)
+        return fig
+
+    def start_capture(self, cb=None):
+        self._capture_cb = cb
+        if self._running:
+            return
+        self._running = True
+        asyncio.get_event_loop().create_task(self.capture_coro())
+
+    def stop_capture(self):
+        self._running = False
 
 
 class PicoScope:
@@ -43,7 +101,7 @@ class PicoScope:
                 ax.plot(*data)
                 self._capture_cb(fig)
 
-    def oneshot(self):
+    def singleshot(self):
         x = np.linspace(0, 5, 20)
         y = np.linspace(0, 5, 20)
         fig, ax = plt.subplots(figsize=(1000 / 72, 400 / 72))
@@ -66,7 +124,8 @@ class PicoScope:
 
 def main():
     plt.style.use("dark_background")
-    scope = PicoScope()
+    # scope = PicoScope()
+    scope = DummyScope()
     with qw.Blocks() as app:
         with qw.Row():
             plot = qw.Plot(scale=3)
@@ -75,7 +134,7 @@ def main():
                     label="Voltage range", value="0.1", choices=["0.1", "1", "10"]
                 )
         with qw.Row():
-            oneshot = qw.Button("One shot")
+            singleshot = qw.Button("Single shot")
         with qw.Row():
             btn_start = qw.Button("Start")
             btn_stop = qw.Button("Stop")
@@ -83,7 +142,7 @@ def main():
     def update_plot(data):
         plot.value = data
 
-    oneshot.click(fn=scope.oneshot, outputs=plot)
+    singleshot.click(fn=scope.singleshot, outputs=plot)
     btn_start.click(fn=lambda: scope.start_capture(cb=update_plot))
     btn_stop.click(fn=scope.stop_capture)
 
